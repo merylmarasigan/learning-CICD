e780e8c8acca551f9cc030d2cb8f929b7a931490
Meryl Marasigan <meryl0729@gmail.com>
2025-07-24 20:34:14 -0700

    Added multiply function and test

--- test_my_function.py
+++ test_my_function.py
@@ -1,6 +1,11 @@
-from my_functions import add
+from my_functions import add, multiply
 
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
+    
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-1, 5) == -5
+    assert multiply(0, 10) == 0
